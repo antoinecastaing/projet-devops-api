021918ddc0cee9fd9b43fb1fc8edb37ca1dafd17
Antoine Castaing <antoinecastaing@MacBook-Pro-de-Antoine.local>
2023-11-13 16:19:28 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,26 @@
+# Projet DevOps API
+
+### Linter:
+eslinter
+
+### Formatter:
+prettier
+
+### Langage:
+typescript
+
+### Packet manager:
+npm
+
+### Dependances:
+Aucune
+
+
+## Lancer le projet
+
+- Télécharger le dossier xxxxx
+- Lancer un terminal
+- Se placer dans le dossier xxxxx
+- Lancer la commande export PING_LISTEN_PORT=number pour que le serveur écoute au port number (3000 par défaut)
+- Lancer la commande "npx tsc"
+- Lancer la commande "node build/index.js"
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import http from "http";
+
+// Définition d'une variable d'environnement pour choisir le port d'écoute du serveur
+const port = process.env.PING_LISTEN_PORT || 3000;
+
+// Création du serveur
+const server = http.createServer((req, res) => {
+  // Renvoie une réponse aux requêtes GET contenant les headers, sinon renvoie une réponse 404 vide
+  if (req.method === "GET" && req.url === "/ping") {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+
+    const responseData = req.headers;
+
+    res.end(JSON.stringify(responseData));
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
+
+// Activation de l'écoute du serveur au port désiré
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
