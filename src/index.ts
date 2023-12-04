d70bceb126912f19129fd23ee86787e8858f766e
Antoine Castaing <antoinecastaing@MacBook-Pro-de-Antoine.local>
2023-12-04 11:57:33 +0100

    tp3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import http from "http";
+const os = require('os');
 
 // Définition d'une variable d'environnement pour choisir le port d'écoute du serveur
 const port = process.env.PING_LISTEN_PORT || 3000;
@@ -7,6 +8,7 @@ const port = process.env.PING_LISTEN_PORT || 3000;
 const server = http.createServer((req, res) => {
   // Renvoie une réponse aux requêtes GET contenant les headers, sinon renvoie une réponse 404 vide
   if (req.method === "GET" && req.url === "/ping") {
+    console.log(os.hostname())
     res.setHeader("Content-Type", "application/json");
     res.statusCode = 200;
 
